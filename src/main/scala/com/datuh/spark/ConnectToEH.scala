711e1a738d0e2f6832dd7483f95c3f9770487412
stevekludt <stevekludt@gmail.com>
2015-12-12 02:41:49 -0600

    refactored into functions and got it to build

--- src/main/scala/com/datuh/spark/ConnectToEH.scala
+++ src/main/scala/com/datuh/spark/ConnectToEH.scala
@@ -3,28 +3,29 @@
   */
 package main.scala.com.datuh.spark
 
-import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.{Row, DataFrame, SQLContext}
-import org.apache.spark.streaming.dstream.DStream
-import org.apache.spark.{SparkConf, _}
-import org.apache.spark.streaming.{StreamingContext, _}
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.{SparkContext, SparkConf}
+import org.apache.spark.streaming.{Seconds, StreamingContext}
 import org.apache.spark.streaming.eventhubs.EventHubsUtils
-import org.json4s.jackson.JsonMethods._
-import org.json4s._
-
 
 object ConnectToEH {
-  implicit val formats = DefaultFormats
+  //implicit val formats = DefaultFormats
 
-  case class Event(deviceName: String, Temperature: Double, Humidity: Double, EventDateTime: String)
+  case class Event(deviceName: String,
+                   Temperature: Double,
+                   Humidity: Double,
+                   EventDateTime: String,
+                   EventProcessedUtcTime: String,
+                   PartitionId: String,
+                   EventEnqueuedUtcTime: String)
 
   def main(args : Array[String]): Unit = {
     //EventHub Parameters
     val ehParams = Map[String, String](
       "eventhubs.policyname" -> "Listen",
-      "eventhubs.policykey" -> "hFg939EylJQv6M0NgyAz84ttznkvhBOMFHfg9F3QztE=",
-      "eventhubs.namespace" -> "datuh-ns",
-      "eventhubs.name" -> "sparkeh",
+      "eventhubs.policykey" -> "C9m+sh8h5jaD4NQqYs1eXKtTDZ7mI9D9Xjqxsr5wML4=",
+      "eventhubs.namespace" -> "datuheh-ns",
+      "eventhubs.name" -> "datuhdev",
       "eventhubs.partition.count" -> "2",
       "eventhubs.consumergroup" -> "$default",
       "eventhubs.checkpoint.dir" -> "/sparkchelpoint",
@@ -38,10 +39,10 @@ object ConnectToEH {
     val stream = EventHubsUtils.createUnionStream(ssc, ehParams)
     import sqlContext.implicits._
 
-    val stringStream: DStream[String] = stream.map(_.toString)
+    val stringStream = stream.map(_.toString)
 
-    val sensorDStream: DStream[Array[String]] = stringStream.map(s =>s.split(","))
-    val sensorDstream2: DStream[Event] = sensorDStream.map(e=>Event(e(0), e(1).trim.toDouble, e(2).trim.toDouble, e(3)))
+    val sensorDStream = stringStream.map(s =>s.split(",")).filter(_.length != 0)
+    val sensorDstream2 = sensorDStream.map(e=>Event(e(0), e(1).trim.toDouble, e(2).trim.toDouble, e(3), e(4), e(5), e(6)))
 
     sensorDstream2.foreachRDD(rdd=>rdd.toDF().registerTempTable("MyEvents"))
     //case class Message(msg: String)
