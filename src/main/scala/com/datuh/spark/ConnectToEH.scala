3903f7576832b4b71a13ab5876a7f3073fa59174
stevekludt <stevekludt@gmail.com>
2015-12-12 00:20:38 -0600

    refactored into functions and got it to build

--- src/main/scala/com/datuh/spark/ConnectToEH.scala
+++ src/main/scala/com/datuh/spark/ConnectToEH.scala
@@ -3,7 +3,8 @@
   */
 package main.scala.com.datuh.spark
 
-import org.apache.spark.sql.SQLContext
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.{Row, DataFrame, SQLContext}
 import org.apache.spark.streaming.dstream.DStream
 import org.apache.spark.{SparkConf, _}
 import org.apache.spark.streaming.{StreamingContext, _}
@@ -15,19 +16,7 @@ import org.json4s._
 object ConnectToEH {
   implicit val formats = DefaultFormats
 
-  //define the class of the incoming json
-  case class Event(EventDateTime: String, Humidity: Double, Temperature: Double, deviceName: String)
-
-  //This is the parser that will parse the Json and apply it to the Event Class
-  def parser(json: String): Event = {
-    implicit val formats = DefaultFormats
-
-    val parsedJson = parse(json)
-    val m = parsedJson.extract[Event]
-    m
-  }
-
-  def b2s(a: Array[Byte]): String = new String(a)
+  case class Event(deviceName: String, Temperature: Double, Humidity: Double, EventDateTime: String)
 
   def main(args : Array[String]): Unit = {
     //EventHub Parameters
@@ -47,17 +36,17 @@ object ConnectToEH {
     val ssc = new StreamingContext(sc, Seconds(10))
     val sqlContext = new SQLContext(sc)
     val stream = EventHubsUtils.createUnionStream(ssc, ehParams)
-
     import sqlContext.implicits._
-    val streamString =  stream.map(b2s)
 
-    val DeviceName: DStream[Event] = streamString.map(parser)
+    val stringStream: DStream[String] = stream.map(_.toString)
 
-    val df = DeviceName.foreachRDD(rdd => rdd.toDF().registerTempTable("Events"))
+    val sensorDStream: DStream[Array[String]] = stringStream.map(s =>s.split(","))
+    val sensorDstream2: DStream[Event] = sensorDStream.map(e=>Event(e(0), e(1).trim.toDouble, e(2).trim.toDouble, e(3)))
 
-    val currentEvents = sqlContext.sql("Select * from Events")
-    currentEvents.collect().foreach(println)
-
+    sensorDstream2.foreachRDD(rdd=>rdd.toDF().registerTempTable("MyEvents"))
+    //case class Message(msg: String)
+    //val sStream = stream.map(msg=>Message(new String(msg))).foreachRDD(rdd=>rdd.toDF().registerTempTable("events"))
+    sensorDstream2.print
     ssc.start()
     ssc.awaitTermination()
   }
